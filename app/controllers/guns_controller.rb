ee35aa35a0458c8aae4afb0f89ffabc143b0be04
Nino Raymond <nino.raymond@hec.edu>
2018-11-26 18:45:27 +0100

    a chier ptn

---
+++ app/controllers/guns_controller.rb
@@ -0,0 +1,38 @@
+class GunsController < ApplicationController
+# skip_before_action :authenticate_user!
+
+#   def Index
+#     @guns = Gun.all
+#     @user = current_user
+#   end
+
+#   def new
+#       @gun = Gun.new
+#   end
+
+#   def show
+#     @gun = Gun.find(params[:id])
+#   end
+
+#   def create
+#     @gun = Cocktail.new
+#     @gun.user = @user
+#     if @gun.save
+#       redirect_to gun_path(@gun)
+#     else
+#       render :new
+#     end
+#   end
+
+#   private
+
+#   def set_gun
+#     @gun = Gun.find(params[:id])
+#   end
+
+#   def gun_params
+#     params.require(:gun).permit(:name, :category, :description, :price, :year_of_production)
+#   end
+# end
+
+end
