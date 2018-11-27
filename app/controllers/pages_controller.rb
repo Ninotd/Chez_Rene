0ebddaa6adf9c1465464ffa5f1f49fa917b8cfb3
Nino Raymond <nino.raymond@hec.edu>
2018-11-27 12:14:58 +0100

    authorization done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
-  # def home
+  def home
   #   # @top_guns #coder le top 3 des guns
-  # end
+  end
 
 end
