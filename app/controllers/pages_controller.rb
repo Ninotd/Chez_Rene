b74361e105e8eff1c60168d4ca9ed16f6c554d2d
victor desjardins <desjardins.victor@gmail.com>
2018-11-27 12:15:22 +0100

    Simple booking_reviews model created as well as booking_reviews_policy file without anything in it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
   # def home
   #   # @top_guns #coder le top 3 des guns
   # end
-
 end
