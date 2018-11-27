727d27e1504be061606f6860b8d4e4d21b22d55c
PaulAntoine Huilizen <paul.antoine.huilizen@gmail.com>
2018-11-27 11:02:08 +0100

    responsive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: :home
 
   # def home
   #   # @top_guns #coder le top 3 des guns
